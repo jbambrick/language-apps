66d26d84b4292721548bac1e36be3acb461ae536
aaron-plahn <aplahn85@gmail.com>
2021-02-18 09:35:14 -0800

    feat(memory-match): implement home page component

--- projects/memory-match/src/app/components/pages/home/home.component.ts
+++ projects/memory-match/src/app/components/pages/home/home.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  title = 'Tŝilhqot’in Memory Match';
 
-  constructor() { }
+  constructor(private router: Router) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
+  navigateToMenu(){
+    this.router.navigateByUrl('/menu');
+  }
 }
