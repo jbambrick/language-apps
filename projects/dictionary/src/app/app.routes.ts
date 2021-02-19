c002e4465f14e75c4d1faf178e9ba45221b30763
aaron-plahn <aplahn85@gmail.com>
2021-02-19 08:34:07 -0800

    feat(dictionary): add logic to load entire vocab list

--- projects/dictionary/src/app/app.routes.ts
+++ projects/dictionary/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/pages/home/home.component';
+import { ListComponent } from './components/pages/list/list.component';
 
 export const routes: Routes = [
-    {"path":"", "component": HomeComponent}
+    {"path":"", "component": HomeComponent},
+    {"path":"lists/:id", "component": ListComponent}
 ];
--- projects/dictionary/src/app/components/pages/list/list.component.ts
+++ projects/dictionary/src/app/components/pages/list/list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { map } from 'rxjs/operators';
+import { DictionaryService } from '../../../services/dictionary/dictionary.service';
 
 @Component({
   selector: 'app-list',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
-  constructor() { }
+  terms: any;
+  listID: string;
+
+
+  constructor( private dictionaryService: DictionaryService, private route: ActivatedRoute ) { }
 
   ngOnInit(): void {
+    this.route.params
+    .pipe(map(params=>params['id']))
+    .subscribe((id:string)=>{
+      this.listID = id;
+    });
+
+    this.dictionaryService.getTermsForListByListID("1").subscribe((terms: any)=>{
+      this.terms = terms;
+      for(let term of terms){
+        console.log(`id for this term is: ${term.id}`);
+      }
+    })
   }
 
 }
