7424476e097956c2bb8fc28fd0c37dca9fb42eaa
aaron-plahn <aplahn85@gmail.com>
2021-02-20 10:42:41 -0800

    feat(dictionary/dropdown): update dropdown to accept generic input

--- projects/dictionary/src/app/components/pages/home/home.component.ts
+++ projects/dictionary/src/app/components/pages/home/home.component.ts
@@ -1,11 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+import { DropdownData } from '../../widgets/dropdown/dropdown-data';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+
+  dropdownData: DropdownData<string> = {
+    "prompt": "favourite food: ",
+    "items": [
+      {"value": "P", "display": "Pizza"},
+      {"value": "S", "display": "Soup"},
+      {"value": "B", "display": "Burgers"}
+    ]
+  }
 
   constructor() { }
 
--- projects/dictionary/src/app/components/widgets/dropdown/dropdown.component.ts
+++ projects/dictionary/src/app/components/widgets/dropdown/dropdown.component.ts
@@ -1,28 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Input, Output, EventEmitter } from '@angular/core';
 
-type DropdownItem<T> = {
-  'value': T,
-  'display': string
-}
-type DropdownData<T> = {
-  'prompt': string,
-  'items': DropdownItem<T>[]
-}
+import { DropdownData } from './dropdown-data';
 
 @Component({
   selector: 'app-dropdown',
   templateUrl: './dropdown.component.html',
   styleUrls: ['./dropdown.component.css']
 })
-export class DropdownComponent implements OnInit {
-  dropdownData: DropdownData<string> = {
-    'prompt': 'favourite food',
-    'items':   [
-      {value: 'steak-0', display: 'Steak'},
-      {value: 'pizza-1', display: 'Pizza'},
-      {value: 'tacos-2', display: 'Tacos'}
-    ]
-  };
+export class DropdownComponent<T> implements OnInit {
+
+  @Input() public set dropdownData(data: DropdownData<T>){
+    console.log(`Setting dropdown data as: ${data}`);
+    this._data = data;
+  }
+
+  _data: DropdownData<T>;
 
   constructor() { }
 
