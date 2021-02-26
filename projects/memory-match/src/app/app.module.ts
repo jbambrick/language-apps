21438243bd04f8e1efe1f9cbaf2c4d2126fee182
aaron-plahn <aplahn85@gmail.com>
2021-02-25 16:02:50 -0800

    feat(memory-match/menu): add roundDisplay pipe to present round name in menu

--- projects/memory-match/src/app/app.module.ts
+++ projects/memory-match/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/pages/home/home.component';
 import { MenuComponent } from './components/pages/menu/menu.component';
 import { GameComponent } from './components/pages/game/game.component';
+import { RoundDisplayPipe } from './pipes/round-display/round-display.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     MenuComponent,
-    GameComponent
+    GameComponent,
+    RoundDisplayPipe
   ],
   imports: [
     BrowserModule,
